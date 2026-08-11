bb089b32557be26ecb4bb9b10104bd6b9d3d3d55
wlals0306 <s25jmini@gmail.com>
2026-08-11 13:18:37 +0900

    fix port

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 
+import os
+
 app = Flask(__name__)
 
 policies = [
@@ -115,4 +117,5 @@ def get_policy():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
